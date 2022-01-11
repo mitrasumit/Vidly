c85753d63df058e9dedfbb3afb936ffd01e898d5
mitra <mitra@SUMIT>
2022-01-12 00:26:28 +0530

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers")]
+        public ViewResult Index()
+        {
+            var customer = GetCustomer();
+
+            return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomer()
+        {
+
+            var customer = new List<Customer>
+            {
+                new Customer{Id=1, Name="Sumit Mitra"},
+                new Customer{Id=2, Name="Rishav Sarkar"}
+            };
+
+            return customer;
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomer().SingleOrDefault(c => c.Id == id);   //SingleorDefault returns a sngle value if present or returns null
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+    }
+}
